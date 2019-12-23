908686b445aa6c68dc464c04e1971ab8b294aa7d
degonismi <43546985+degonismi@users.noreply.github.com>
2019-12-23 11:20:57 +0300

    init

---
+++ Assets/Scripts/GamePlay/Block.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Stop");
+    }
+}
---
+++ Assets/Scripts/GamePlay/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+    
+}
